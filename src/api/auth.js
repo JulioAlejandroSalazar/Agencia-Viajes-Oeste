ba520b9e0fddf4ee0ff7c35d4698c043b77b0248
Julio <julio522002@gmail.com>
2026-01-10 09:13:28 -0300

    minor updates

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,11 +1,11 @@
 export async function login(email, password) {
     /*
       Nota:
-      ReqRes es un servicio de prueba y no permite ser consumido directamente
-      desde el frontend en todos los entornos. Para poder implementar y
-      demostrar el flujo de autenticacion solicitado en la actividad,
+      ReqRes no permite ser consumido directamente
+      desde el frontend en todos los entornos. Para poder implementar
+      la autenticacion solicitada en la actividad,
       se simula la respuesta utilizando las credenciales de ejemplo
-      proporcionadas por ReqRes.
+      de ReqRes.
   
       Esta logica puede reemplazarse por una conexión real
       a un backend.
